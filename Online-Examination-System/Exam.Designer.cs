160af34f5b76930bc9e71a38dbb1bdf09a8787b9
Yousef-Farouk <yfrouk853@gmail.com>
2024-03-06 15:15:56 +0200

    local changes

---
+++ Online-Examination-System/Exam.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Online_Examination_System
+{
+    partial class Exam
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            generate = new Button();
+            SuspendLayout();
+            // 
+            // generate
+            // 
+            generate.Location = new Point(465, 29);
+            generate.Name = "generate";
+            generate.Size = new Size(176, 47);
+            generate.TabIndex = 2;
+            generate.Text = "RequestExam";
+            generate.UseVisualStyleBackColor = true;
+            generate.Click += generate_Click;
+            // 
+            // Exam
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            AutoScroll = true;
+            BackColor = SystemColors.Window;
+            ClientSize = new Size(1185, 763);
+            Controls.Add(generate);
+            Name = "Exam";
+            Padding = new Padding(10);
+            Text = "Exam";
+            ResumeLayout(false);
+        }
+
+        #endregion
+        private Button generate;
+    }
+}
---
+++ Online-Examination-System/PreviousExams.cs
@@ -0,0 +1,52 @@
+﻿using Online_Examination_System.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Online_Examination_System
+{
+    public partial class PreviousExams : Form
+    {
+        OnlineExaminatonSystemContext context = new OnlineExaminatonSystemContext();
+        public PreviousExams()
+        {
+            InitializeComponent();
+        }
+
+        private void PreviousExams_Load(object sender, EventArgs e)
+        {
+            var exams = context.StudentCourseExam.Where(s=> s.St_Id == 1).ToList();
+            int y = 50;
+            int x = 50;
+            foreach (var exam in exams) 
+            {
+                Panel p = new Panel();
+                p.Location = new System.Drawing.Point(x, y);
+                p.AutoSize = true;
+                //p.Size = new System.Drawing.Size(200, 500);
+                Controls.Add(p);
+                var CourseName = new Label();
+                string passed = "";
+                passed = exam.Passed == 1 ? "Passed" : "Failed";
+
+                CourseName.AutoSize = true;
+                CourseName.Text = $"Course: {exam.Course.Name}        Grade: {exam.Grade.ToString()}        Status:{passed}";
+                CourseName.Font = new Font("Arial", 20);
+                //CourseName.Location = new Point(10, 10);
+                p.Controls.Add(CourseName);
+
+                y += p.Height + 30;
+            }
+
+        }
+
+
+    }
+}
